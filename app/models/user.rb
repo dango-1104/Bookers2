393582c4f03083d8a491f63e8e1461f7b570f82e
dango-1104 <570401mn@gmail.com>
2021-06-06 15:09:51 +0000

    10th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,25 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+    # model/user.rb
+  # ユーザーをフォローする
+  def follow(user_id)
+   follower.create(followed_id: user_id)
+  end
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+   follower.find_by(followed_id: user_id).destroy
+  end
+  # フォロー確認をおこなう
+  def following?(user)
+   following_user.include?(user)
+  end
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
